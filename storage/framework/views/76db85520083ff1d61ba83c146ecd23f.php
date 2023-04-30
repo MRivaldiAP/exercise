2314536979e15470a55922f1cbb7c7dd9221d1ee
MRivaldiAP <113491822+MRivaldiAP@users.noreply.github.com>
2023-04-30 21:26:55 +0700

    Add files via upload

---
+++ storage/framework/views/76db85520083ff1d61ba83c146ecd23f.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+
+    <title>Latihan</title>
+
+    <!-- Favicons -->
+    <link href="<?php echo e(asset('assets/img/favicon.png')); ?>" rel="icon">
+    <link href="<?php echo e(asset('assets/img/apple-touch-icon.png')); ?>" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="<?php echo e(asset('assets/vendor/aos/aos.css')); ?>" rel="stylesheet">
+    <link href="<?php echo e(asset('assets/vendor/bootstrap/css/bootstrap.min.css')); ?>" rel="stylesheet">
+    <link href="<?php echo e(asset('assets/vendor/bootstrap-icons/bootstrap-icons.css')); ?>" rel="stylesheet">
+    <link href="<?php echo e(asset('assets/vendor/boxicons/css/boxicons.min.css')); ?>" rel="stylesheet">
+    <link href="<?php echo e(asset('assets/vendor/glightbox/css/glightbox.min.css')); ?>" rel="stylesheet">
+    <link href="<?php echo e(asset('assets/vendor/remixicon/remixicon.css')); ?>" rel="stylesheet">
+    <link href="<?php echo e(asset('assets/vendor/swiper/swiper-bundle.min.css')); ?>" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="<?php echo e(asset('assets/css/style.css')); ?>" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Header -->
+    <header>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand navbar-light fixed-top ">
+            <a href="#" class="navbar-brand"><h1 class="text-primary">Latihan</h1></a>
+            <ul>
+                <li class="nav-item active">
+                    <a href="#" class="nav-link text-light">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="text-light dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                        Gallery
+                      </a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <a href="#" class="dropdown-item text-primary">Gamis</a>
+                        <a href="#" class="dropdown-item text-primary">Burkat</a>
+                        <a href="#" class="dropdown-item text-primary">Jilbab</a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+        <!-- Navbar End -->
+
+        <!-- Background Image -->
+        <div class="text-center bg-image" style="background-image: url('assets/img/image/mountain.webp'); background-size:100%">
+            <div class="mask" style="background-color: rgba(0, 0, 0, 0.6); height:700px">
+                <div class="d-flex justify-content-center align-items-center h-100">
+                    <div class="text-white" data-aos="fade-up">
+                        <h1 class="mb-3">Latihan</h1>
+                        <h4 class="mb-3">Item Latihan</h4>
+                        <button type="button" class="btn btn-outline-light btn-lg">LEARN MORE</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <!-- Background Image End -->
+    </header>
+    <!-- Header End -->
+    <!-- Main -->
+    <main>
+        <section id="gallery" class="gallery">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="d-flex flex-column col-lg-7 alignment-items-stretch justify-content-center">
+                        <div class="p-5">
+                            <h2>Have a Look at Our Collections!</h2>
+                            <p>Brought to you from the most fresh and fashionable local brand all around Indonesia</p>
+                            <img src="assets/img/image/hammer.jpg" alt="geological hammer" data-aos="fade-up" style="height:500px">
+                        </div>
+                    </div>
+                    <div class="d-flex flex-column justify-content-center alignment-items-stretch col-lg-5 ">
+
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+    </main>
+    <!-- Main End -->
+      <!-- Vendor JS Files -->
+  <script src="<?php echo e(asset('assets/vendor/aos/aos.js')); ?>"></script>
+  <script src="<?php echo e(asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')); ?>"></script>
+  <script src="<?php echo e(asset('assets/vendor/glightbox/js/glightbox.min.js')); ?>"></script>
+  <script src="<?php echo e(asset('assets/vendor/isotope-layout/isotope.pkgd.min.js')); ?>"></script>
+  <script src="<?php echo e(asset('assets/vendor/swiper/swiper-bundle.min.js')); ?>"></script>
+  <script src="<?php echo e(asset('assets/vendor/waypoints/noframework.waypoints.js')); ?>"></script>
+  <script src="<?php echo e(asset('assets/vendor/php-email-form/validate.js')); ?>"></script>
+
+  <!-- Template Main JS File -->
+  <script src="<?php echo e(asset('assets/js/main.js')); ?>"></script>
+</body>
+
+</html>
+<?php /**PATH D:\M. Rivaldi Anwar Putra\latihan\resources\views/welcome.blade.php ENDPATH**/ ?>
